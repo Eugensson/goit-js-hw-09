7b4890cc53fd37890661b39cf281a5d4a6f5a4f9
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-02-24 23:25:11 +0200

    Внесення змін до таску № 1 згідно зауваження ментара

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -17,5 +17,7 @@ stopBtn.addEventListener('click', () => {
 });
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
